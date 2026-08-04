6208da914f671a9a1f7cdd120a6c25fbc127b2fb
ArtemSh51 <128077115+ArtemSh51@users.noreply.github.com>
2026-08-04 14:48:34 +0300

    Updated_UI_001

--- Assets/Scripts/UI/CanvasPursuer.cs
+++ Assets/Scripts/UI/CanvasPursuer.cs
@@ -2,11 +2,14 @@ using UnityEngine;
 
 public class CanvasPursuer : MonoBehaviour
 {
+    private const int HeightDivider = 2;
+
     [SerializeField] private Transform _target;
+    [SerializeField] private float _heightAboveTarget;
 
     private void Update()
     {
-        float y = _target.position.y + _target.localScale.y / 2 + 1;
-        transform.position = new Vector3(_target.position.x, y, _target.position.z);
+        float yPosition = _target.position.y + _target.localScale.y / HeightDivider + _heightAboveTarget;
+        transform.position = new Vector3(_target.position.x, yPosition, _target.position.z);
     }
 }
